761f7d6fb108bb0293aa322480e2905a514eef60
Shanaa Modchalingam <s.modcha@gmail.com>
2018-12-21 18:04:50 -0500

    Added R project file

---
+++ reachData_preprocessing.R
@@ -0,0 +1,7 @@
+## Proprocessing of Raw Reach Data
+
+# Read in data from the hand-tracker output files
+
+# Determine a relevant measure per trial (e.g. angle at max velocity)
+
+# Output a CSv file with per trial data (perhaps also merge the per-trial file made by UxF)
